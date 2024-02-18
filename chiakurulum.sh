f4a9fe433e76c7511a87a18aedbad1d59046f46b
toprak016838 <159837755+toprak016838@users.noreply.github.com>
2024-02-18 17:08:45 +0300

    satis

---
+++ chiakurulum.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# apt-get update
+
+dpkg --configure -a
+apt update -y
+apt upgrade -y 
+apt-get update -y 
+apt-get upgrade -y
+sudo apt-get install git -y
+sudo apt-get install make -y
+sudo apt-get install screen -y
+sudo apt --reinstall install fuse -y 
+sudo apt-get install unzip -y
+
+mkdir /root/plt
+
+
+
+unzip kurulum.zip
+
+cd ~
+
+rm -rf kurulum.zip
+
+cd ~ 
+
+apt-get install -y nano unzip zip curl sshpass python3 python3-pip screen git
+
+cd ~
+
+mkdir /root/.config
+mkdir /root/.config/rclone
+mv kurulum/rclone.conf .config/rclone
+mv kurulum/accounts .config/rclone
+cd
+
+
+
+sudo -v ; curl https://rclone.org/install.sh | sudo bash
+
+
+sudo apt-get install -y rand
+
+cd ~
+mv /root/kurulum/x.sh /root
+mv /root/kurulum/automadmax.sh /root
+screen -dmS oto bash -c 'bash automadmax.sh'
+
+screen -dmS plot bash -c 'bash x.sh'
+
+mv /root/kurulum/upload.sh /root/plt/
+cd /root/plt
+screen -dmS upload bash -c 'bash upload.sh'
+
+
+cd ~
+apt update -y
+screen -x plot
+cd ~
+
